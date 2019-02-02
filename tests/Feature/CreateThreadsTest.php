cd82e1e9a5d184b1ccf6f22237bfa7fc34f5ac47
Jeff <jzinger100@gmail.com>
2019-02-02 15:21:14 +0100

    #8 Better exception handling

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -9,10 +9,19 @@ class CreateThreadsTest extends DBTestCase
     /** @test */
     public function guests_may_not_create_threads()
     {
-        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $this->withoutExceptionHandling()
+            ->expectException('Illuminate\Auth\AuthenticationException');
 
         $thread = make('App\Thread');
         $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    public function guests_cannot_see_create_thread_page()
+    {
+        $this->withExceptionHandling()
+            ->get('/threads/create')
+            ->assertRedirect('/login');
     }
 
     /** @test */
